e7b519c2bb3392731578a326fc7e2acf6aa37337
kishore.k1024@gmail.com <kishore.k1024@gmail.com>
2019-11-13 10:36:06 +0530

    Commit1

---
+++ WebDriverLatest/src/pack/FirstClass1.java
@@ -0,0 +1,29 @@
+package pack;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+
+public class FirstClass1 {
+
+	public static void main(String[] args) {
+		
+		/*System.setProperty("webdriver.gecko.driver", "C:\\Users\\DELL\\Desktop\\WD1010\\Jars\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://facebook.com");
+		driver.manage().window().maximize();*/
+
+		/*System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\WD1010\\Jars\\chromedriver.exe");
+		WebDriver driver1=new ChromeDriver();
+		driver1.get("http://yahoomail.com");
+		driver1.manage().window().maximize();*/
+		
+		
+		
+		
+	}
+
+}
